33d28ff9f2fd98d021cdb3d6051d75bfe817776e
Solar Flower - Vikasa <36963611+SolarFlower-Vikasa@users.noreply.github.com>
2018-03-04 23:15:27 -0500

    Update VikasaSolarTracker.py

--- VikasaSolarTracker.py
+++ VikasaSolarTracker.py
@@ -1,45 +1,6 @@
 '''
 Solar Tracking Code for Vikasa
 
-Written and Developed by Zachary Switzer copyright February 14th 2018.
-Duplication or modification of this code can only be used for educational or
-non-profit projects, or for any projects with the original developer's
-(Zach Switzer) consent.
-
-Vikasa translates to the opening or closing of a flower in Sanskrit, and is a
-project developed at the George Washington University by Zach Switzer,
-Ali Ahmed, Feng (Jason) Xiang, Aaron Patron, and Jacari Matthews. It is a
-portable solar charging unit designed and created in order to tackle the issue
-of access to clean and renewable power that arises in developing nations.
-It incorperates a Raspberry Pi Zero W as a computer for those in developing
-nations who do not already have a computer and as a controller for the
-function of the system as a whole, a solar array that expands and contracts
-through origami folding, and a battery pack with DC USB access. This project
-was supported and funded by Amar Hanspal, Makerbot, and Ninjatek. Special
-thanks to mentors who provided support and inspiration along the way including:
-Shannon Zirbel, Larry Howell, Megan Leftwich, and Kevin Patton. Very special
-thanks to Richard G. Hirst for the creation of ServoBlaster and Brandon
-Stafford for the creation of Pysolar, without which this portion of the project
-would not have come to fruition.
-
-Pysolar can be found at:
-http://pysolar.org/ and https://github.com/pingswept/pysolar
-
-ServoBlaster can be found at:
-https://github.com/richardghirst/PiBits/tree/master/ServoBlaster
-
-This code was developed in Python 3 for a dual axis solar tracking system with
-360 degree rotation in the x axis and 180 degree rotation in the y axis. It is
-meant to be used with 2 servo motors (each rotate 180 degrees) and employs the
-Pysolar and ServoBlaster open source programs in order to track the sun's
-movement and mirror it with motors.
-
-This code was developed with the intent of being used on a raspberry pi and
-as a result it is assumed that both Pysolar and ServoBlaster have already been
-downloaded onto the modified (for Pysolar) linux computer and can be called
-upon and executed at will. For total system autonomy execute on the raspberry
-pi's bootup sequence.
-
 This code was created for execution in Python3 and will not work in other
 versions of Python (Pysolar only works when executed in Python3).
 '''
@@ -55,13 +16,7 @@ from subprocess import call # Use for turning off the Pi
 
 ServoBlaster = open('/dev/servoblaster', 'w') # opening servoblaster
 
-# issues to resolve:
-#   - need to make sure that x axis servo zero position and y axis sero Zero
-#     position are matched up at the start for accurate pan / tilt
-#   - need to make sure that x axis angles match up to what they are suppose to
-#     be
-
-# code structure: code must be put into boot-up sequence of pi
+# code structure: code must be put into boot-up sequence of pi for automation
 #   - code saying wait 3 min for user interrupt
 #       => if there is interrupt then stop the end the code but keep the pi on
 #       => if there is no interrupt / time runs out run the rest of the code
@@ -171,7 +126,7 @@ rad=radiation.get_radiation_direct(d, alt) # clear sky radiation
 
 General ServoBlaster code
 ServoBlaster.write('P1-11=2500us' + '\n')	# lets you write to file
-# and hit enter
+and hit enter
 ServoBlaster.flush() # lets the info go to the file immediately without it
 # needing to be closed and opened first
 '''

4eaaa090233ebc90fe2fae2b67e3fd0a86c6c745
Solar Flower - Vikasa <36963611+SolarFlower-Vikasa@users.noreply.github.com>
2018-04-20 23:32:13 -0400

    Update VikasaSolarTracker.py

--- VikasaSolarTracker.py
+++ VikasaSolarTracker.py
@@ -40,10 +40,10 @@ for i in range(0,20):
   print(azi)
   print('Number of Iterations: ' + str(count))
   time.sleep(15)
-  if alt>10: # For loop terminates if sun is up
+  if alt>15: # For loop terminates if sun is up
     break
   if count==20:
-    print('The sun never came up. Shutting down.')
+    print('The sun is not high enough. Shutting down.')
     call("sudo shutdown -h now", shell=True) # Raspberry Pi shutdown command
   
 # DC motor code to open array
@@ -66,32 +66,76 @@ d=datetime.now() # want to call this to update sun position
 alt=get_altitude(latitude, longitude, d) # current altitude
 azi=get_azimuth(latitude, longitude, d, elevation) # current azimuth
 
-while (alt>10): # setting the contraints in y axis degrees of motion
-  d=datetime.now() # want to call this to update sun position
-  alt=get_altitude(latitude, longitude, d) # current altitude
-  azi=get_azimuth(latitude, longitude, d, elevation) # current azimuth
-  servo_y=((alt/90)*1000)+500
-  servo_y2=((alt/90)*1000)+650
-  servo_x=((azi/180)*1000)+1500 # Put servo in center position
+while (azi<=105 and azi>=-105): 
+  d=datetime.now() # want to call this to update sun
+  alt=get_altitude(latitude, longitude, d) 
+  azi=get_azimuth(latitude, longitude, d, elevation) 
+  servo_x=((azi/180)*1000)+1500 
   servo_x2=((azi/180)*1000)+1650
+  servo_y=((alt/90)*845)+625
+  servo_y2=1470
   ServoBlaster.write('P1-11=' + str(servo_y2) + 'us' + '\n') # pulse to pin 11
   ServoBlaster.flush()
   time.sleep(5)
-  ServoBlaster.write('P1-11=' + str(servo_y) + 'us' + '\n') # pulse to pin 12
-  ServoBlaster.flush()
-  time.sleep(5)
+  servo_y3=servo_y+100
+  w=(servo_y2-servo_y3)/5
+  x=(servo_y2-servo_y3)/4
+  y=(servo_y2-servo_y3)/3
+  z=(servo_y2-servo_y3)/2
   ServoBlaster.write('P1-12=' + str(servo_x2) + 'us' + '\n') # pulse to pin 11
   ServoBlaster.flush()
   time.sleep(5)
-  ServoBlaster.write('P1-12=' + str(servo_x) + 'us' + '\n') # pulse to pin 12
+  ServoBlaster.write('P1-12=' + str(servo_x) + 'us' + '\n') # pulse to pin 11
   ServoBlaster.flush()
   time.sleep(5)
+  print (servo_y2)
+  if (servo_y2-servo_y3)>=500:
+    for i in range(0,5):
+      servo_y2=servo_y2-w
+      ServoBlaster.write('P1-11=' + str(servo_y2) + 'us' + '\n') # pulse to pin 12
+      ServoBlaster.flush()
+      time.sleep(0.5)
+  elif (servo_y2-servo_y3)>=400:
+    for i in range(0,4):
+      servo_y2=servo_y2-x
+      ServoBlaster.write('P1-11=' + str(servo_y2) + 'us' + '\n') # pulse to pin 12
+      ServoBlaster.flush()
+      time.sleep(0.5)
+  elif (servo_y2-servo_y3)>=300:
+    for i in range(0,3):
+      servo_y2=servo_y2-y
+      ServoBlaster.write('P1-11=' + str(servo_y2) + 'us' + '\n') # pulse to pin 12
+      ServoBlaster.flush()
+      time.sleep(0.5)
+  elif (servo_y2-servo_y3)>=200:
+    for i in range(0,2):
+      servo_y2=servo_y2-z
+      ServoBlaster.write('P1-11=' + str(servo_y2) + 'us' + '\n') # pulse to pin 11
+      ServoBlaster.flush()
+      time.sleep(0.5)
+  else:
+    servo_y2=servo_y3
+    ServoBlaster.write('P1-11=' + str(servo_y2) + 'us' + '\n') # pulse to pin 11
+    ServoBlaster.flush()
+    time.sleep(0.5)
+  for i in range(0,1200):
+    ServoBlaster.write('P1-11=' + str(servo_y) + 'us' + '\n') # pulse to pin 11
+    ServoBlaster.flush()
+    time.sleep(0.5)
+  while (servo_y<1270):
+    servo_y=servo_y+100
+    ServoBlaster.write('P1-11=' + str(servo_y) + 'us' + '\n') # pulse to pin 11
+    ServoBlaster.flush()
+    time.sleep(1)
+  servo_y=1470
+  ServoBlaster.write('P1-11=' + str(servo_y) + 'us' + '\n') # pulse to pin 11
+  ServoBlaster.flush()
+  time.sleep(1)
   count=count+1 # Shows how many iterations have been done
   print(count)
   print(str(d) + '\n')
-  print(str(servo_y) + '\n')
-  print(str(servo_x) + '\n')
-  time.sleep(600) # Sleep for 10 min before next iteration
+  print(str(azi) + '\n')
+  print(str(alt) + '\n')
 
 # DC motor code to close array
 ServoBlaster.write('P1-16=2500us' + '\n') # Tell the motor to run in the opposite direction
